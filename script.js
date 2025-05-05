713f20f944e0e359932e59c5503e275f2ae65a94
Shariq Hashmi <127607558+bonkers23@users.noreply.github.com>
2025-05-05 22:18:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,15 @@
-let strongText = document.querySelectorAll("strong");
-
- 
-
 function highlight() {
-
     //Write your code here
-
-    for(let t of strongText){
-
-        t.style.color = "rgb(0, 128, 0)";
-
-    }
-
- 
-
+	let strong=document.querySelectorAll("strong")
+	for(let t of strong){
+		t.style.color="rgb(0, 128, 0)";
+	}
 }
-
- 
-
 function return_normal() {
-
     //Write your code here
-
- 
-
-    for(let t of strongText){
-
-        t.style.color = "rgb(0, 0, 0, 0)";
-
-    }
-
- 
-
+	let strong=document.querySelectorAll("strong")
+	for(let t of strong){
+		t.style.color="rgb(0, 0, 0)";
+	}
     
-
 }
